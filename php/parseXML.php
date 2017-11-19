ad14b39b5e4b1bd22d346e87265858e22eb0631b
tiantud <tianz0422@gmail.com>
2017-11-19 20:27:59 +0100

    Reordered

--- php/parseXML.php
+++
@@ -1,7 +0,0 @@
-<?php
-require ('world_data_parser.php');
-$newParser = new WorldDataParser();
-$dataInArray = $newParser->parseCSV("../Material/world_data_v1.csv");
-$num = count($dataInArray);
-echo "<pre>";print_r($dataInArray);echo "<pre>";
-?>
